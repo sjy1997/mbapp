acc07088374f3329f8cfb547235f73407a18a7a9
sjy1997 <young.sarahjane97@hotmail.com>
2022-07-23 10:59:54 +0100

    updating knowledge base

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -9,7 +9,7 @@ const About = () => (
       <h2 className="title">Need Help?</h2>
       <ul>
         <li>
-            <iframe title='title 'src="https://webchat.botframework.com/embed/qnaknowledgebasesjy-bot?s=8gIuYDkicl0.Cm9-2JoEYtpZ4_-9Om3_4LwaIY9st1fyz4cv7AZP7TM"></iframe>
+            <iframe title='mykb'src="https://webchat.botframework.com/embed/qnaknowledgebasesjy-bot?s=8gIuYDkicl0.Cm9-2JoEYtpZ4_-9Om3_4LwaIY9st1fyz4cv7AZP7TM"></iframe>
 
         </li>
       </ul>
