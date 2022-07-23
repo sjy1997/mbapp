8744ac030948a8aec7f9eaf598ef208592abecd2
sjy1997 <young.sarahjane97@hotmail.com>
2022-07-23 11:15:38 +0100

    updating kb

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles.scss';
 
 
 
@@ -9,7 +10,7 @@ const About = () => (
       <h2 className="title">Need Help?</h2>
       <ul>
         <li>
-            <iframe title='mykb'src="https://webchat.botframework.com/embed/qnaknowledgebasesjy-bot?s=8gIuYDkicl0.Cm9-2JoEYtpZ4_-9Om3_4LwaIY9st1fyz4cv7AZP7TM"></iframe>
+            <iframe title='mykb'id='mykkb' min-width='400px' width='100%' height='500px'src="https://webchat.botframework.com/embed/qnaknowledgebasesjy-bot?s=8gIuYDkicl0.Cm9-2JoEYtpZ4_-9Om3_4LwaIY9st1fyz4cv7AZP7TM" ></iframe>
 
         </li>
       </ul>
