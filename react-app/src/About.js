282f7bda27cbcaa89d433a32798fcb8d86c1b961
sjy1997 <young.sarahjane97@hotmail.com>
2022-07-23 10:56:27 +0100

    adding knowledge base

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
+
+
 const About = () => (
   <div className="content-container">
     <div className="content-title-group not-found">
-      <h2 className="title">Product Wish List</h2>
-      <p>
-        This project was created to help represent a fundamental app written
-        with React. The shopping theme is used throughout the app.
-      </p>
-      <br />
-      <h2 className="title">Resources</h2>
+
+      <h2 className="title">Need Help?</h2>
       <ul>
         <li>
-          <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp">
-            Code in GitHub
-          </a>
+            <iframe title='title 'src="https://webchat.botframework.com/embed/qnaknowledgebasesjy-bot?s=8gIuYDkicl0.Cm9-2JoEYtpZ4_-9Om3_4LwaIY9st1fyz4cv7AZP7TM"></iframe>
+
         </li>
       </ul>
     </div>
   </div>
+
 );
+
 
 export default About;
